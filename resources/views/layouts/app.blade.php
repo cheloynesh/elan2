ea4001e5099409b32b77d1f875586ee3269c9472
cheloynesh <cheloynesh@gmail.com>
2021-03-25 02:34:36 -0600

    plugins y css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,71 +34,69 @@
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link href="{{ asset('public/pluings/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/pluings/bootstrap/css/bootstrap-multiselect.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/pluings/editable/css/bootstrap-editable.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/sb-admin-2.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/color.css') }}" rel="stylesheet"  >
+    <link href="{{ asset('public/pluings/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/pluings/datatable/css/dataTables.bootstrap.min.css') }}" rel="stylesheet" >
+    <link href="{{ asset('public/pluings/multiselect/css/bootstrap-multiselect.css') }}" rel="stylesheet" >
+    <link href="{{ asset('public/plugins/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/pluings/alertify/css/alertify.css') }}" rel="stylesheet" >
+    <link href="{{ asset('public/pluings/alertify/css/default.css') }}" rel="stylesheet" >
+    <link href="{{ asset('public/pluings/chosen/chosen.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/responsive/1.0.0/css/dataTables.responsive.css">
+
+    <style>
+        .dropdown-menu {
+             background-color: #FFFFFF!important;
+        }
+        .dropdown-menu>.active>a, .dropdown-menu>.active>a:focus, .dropdown-menu>.active>a:hover {
+            background-color: #FFFFFF!important;
+            color:#555!important;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
 
-        <!-- jQuery CDN - Slim version (=without AJAX) -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <!-- Popper.JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-        <!-- Bootstrap JS -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script src="{{asset('public/plugins/jquery/jquery-3.1.1.js')}}"></script>
+    <script src="{{asset('public/plugins/jquery/jquery.number.js')}}"></script>
+    <script src="{{asset('public/pluings/datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('public/plugins/select2/js/select2.min.js')}}"></script>
+    <script src="{{asset('public/pluings/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('public/plugins/bootstrap/js/bootstrap-multiselect.js')}}"></script>
+    <script src="{{asset('public/pluings/editable/js/bootstrap-editable.min.js')}}"></script>
+    <script src="{{asset('public/pluings/editable/js/moment.js')}}"></script>
+    <script src="{{asset('public/pluings/editable/js/moment-espanol.js')}}"></script>
+    <script src="{{asset('public/pluings/datatable/js/dataTables.bootstrap.js')}}"></script>
+    <script src="{{asset('public/plugins/multiselect/js/bootstrap-multiselect.js')}}"></script>
+    <script src="{{asset('public/pluings/metisMenu/metisMenu.min.js')}}"></script>
+    <script src="{{asset('public/pluings/alertify/js/alertify.js')}}"></script>
+    <script src="{{asset('public/js/sb-admin-2.js')}}"></script>
+    <script src="{{asset('public/pluings/chosen/chosen.jquery.js')}}"></script>
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <script src="{{asset('public/js/app.js')}}"></script>
+    {{-- @yield('js') --}}
+    <script>
+        $.ajaxSetup({
+            headers:{'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')}
+        });
+    </script>
 </body>
 </html>
