4491e3f65a3c252854fa2c2cba4221fd4300744e
Chelo <dicarloarceo@gmail.com>
2021-08-12 16:36:02 -0500

    front arreglado

--- app/Policy.php
+++ app/Policy.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Policy extends Model
 {
-    
+    use SoftDeletes;
+
+    protected $table = "Initials";
+    protected $fillable =[
+        'fk_client','policy','initial_date','end_date','expended_exp','exp_impute','financ_exp','financ_impute','other_exp','other_impute',
+        'renovable','pay_frec','iva','total'];
+    protected $dates = ["deleted_at"];
 }
